4071104dbeb70df1cf3d3961966f99e5c29b05fe
fabiangunzinger <fa.gunzinger@gmail.com>
2023-05-10 09:05:33 +0100

    Enable find buffers

--- nvim/.config/nvim/lua/plugins/plugins_config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/plugins_config/telescope.lua
@@ -22,6 +22,7 @@ local wk = require("which-key")
 
 local often_used_dirs = {
   "~/.dotfiles",
+  "~/notes/",
   "~/dev/projects/notes/",
   "~/dev/projects/blog/",
 }
@@ -93,19 +94,19 @@ wk.register(
     f = { '<cmd>Telescope find_files<cr>', 'files' },
     g = { '<cmd>Telescop live_grep<cr>', 'grep' },
     b = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "buffer" },
-    -- g = { function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end, "grep" }, h = { '<cmd>Telescope help_tags<cr>', 'help' },
     h = { '<cmd>Telescope help_tags<cr>', 'help tags' },
     k = { '<cmd>Telescope keymaps<cr>', 'keymaps' },
     r = { '<cmd>Telescope lsp_references<cr>', 'references' },
     m = { "<cmd>Telescope marks<cr>", "marks" },
     M = { "<cmd>Telescope man_pages<cr>", "man pages" },
     c = { "<cmd>Telescope git_commits<cr>", "git commits" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "symbols" }, -- d = { "<cmd>Telescope buffers<cr>", "buffers" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "symbols" },
+    d = { "<cmd>Telescope buffers<cr>", "buffers" },
     q = { "<cmd>Telescope quickfix<cr>", "quickfix" },
     l = { "<cmd>Telescope loclist<cr>", "loclist" },
     j = { "<cmd>Telescope jumplist<cr>", "marks" },
     p = { "project" },
-    o = { function() builtin.find_files({ search_dirs = often_used_dirs }) end, "often used files" }
+    o = { function() builtin.find_files({ search_dirs = often_used_dirs }) end, "search often used dirs" }
   },
 }, { mode = 'n', prefix = '<leader>' }
 )
