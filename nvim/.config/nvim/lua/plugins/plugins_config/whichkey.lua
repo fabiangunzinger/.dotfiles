3bd0de3c6e081f98497e0e9f5bbd0e59df90cb90
fabiangunzinger <fa.gunzinger@gmail.com>
2023-05-22 07:19:02 +0100

    Simpler harpoon keybindings

--- nvim/.config/nvim/lua/plugins/plugins_config/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/plugins_config/whichkey.lua
@@ -174,7 +174,7 @@ wk.register(
     h = {
       name = "Harpoon",
       h = { ':lua require("harpoon.ui").toggle_quick_menu()<cr>', "Toggle menu" },
-      m = { ':lua require("harpoon.mark").add_file()<cr>', "Mark file" },
+      a = { ':lua require("harpoon.mark").add_file()<cr>', "Mark file" },
       n = { ':lua require("harpoon.ui").nav_next()<cr>', "Next file" },
       p = { ':lua require("harpoon.ui").nav_prev()<cr>', "Previous file" },
       t = { 'lua require("harpoon.term").gotoTerminal(1)<cr>', "Go to terminal 1" },
