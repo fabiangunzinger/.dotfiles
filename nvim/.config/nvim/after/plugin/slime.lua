6d691384e7ff270891fc9b47b48df09ad1497f67
fabiangunzinger <fa.gunzinger@gmail.com>
2023-06-08 16:22:00 +0100

    Use default slime shortcuts

--- nvim/.config/nvim/after/plugin/slime.lua
+++ nvim/.config/nvim/after/plugin/slime.lua
@@ -1,7 +1,7 @@
 vim.b.slime_cell_delimiter = "#%%"
 
 -- Disable default mappings
-vim.g.slime_no_mappings = 1
+-- vim.g.slime_no_mappings = 1
 
 -- code destination
 -- -- tmux
@@ -12,7 +12,7 @@ vim.g.slime_no_mappings = 1
 vim.g.slime_target = "neovim"
 -- vim.g.slime_python_ipython = 1
 
-vim.keymap.set("n", "<c-cr>", "<Plug>SlimeParagraphSend")
-vim.keymap.set("x", "<cr>", "<Plug>SlimeRegionSend")
-vim.keymap.set("n", "<s-cr>", "<Plug>SlimeLineSend")
-vim.keymap.set("n", "<leader><cr>", "<Plug>SlimeMotionSend")
+-- vim.keymap.set("n", "<c-cr>", "<Plug>SlimeParagraphSend")
+-- vim.keymap.set("x", "<cr>", "<Plug>SlimeRegionSend")
+-- vim.keymap.set("n", "<s-cr>", "<Plug>SlimeLineSend")
+-- vim.keymap.set("n", "<leader><cr>", "<Plug>SlimeMotionSend")
